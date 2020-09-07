1f93317d40b461c9495b52f9146cfabf29628351
chetan <chetan94k.sharma@gmail.com>
2020-09-08 03:48:41 +0530

    added code for googlesheet

--- constants.py
+++ constants.py
@@ -1,15 +1,17 @@
-###Login##
+# Login
 username = 'chetan.sharma@uipath.com'
 password = b'QU9MQGhhcHB5Xzk0NzI5'
 security_token = b'RWNVd3hUN1FpMmpmekJ1SDB1QWNNMU9Y'
 
-###SOQL##
+# SOQL
 query = "SELECT CaseNumber,Subject,Id,Priority,Account_Type__c  FROM Case  WHERE  CaseNumber='"
-
 case_permalink = 'https://uipath.lightning.force.com/lightning/r/Case/{0}/view'
 headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
-###Excel details##
+# Excel details
 file_path = "./output/test.xlsx"
 
-
+# google_sheet
+scopes = ['https://www.googleapis.com/auth/spreadsheets']
+spreadsheet_id = '1jJBJeh8_oreez1dd_fM-BEKsH9gjEdCkKtLuDxqOJEM'
+sheet_range = 'Sheet1!A:A'
--- store_data.py
+++ store_data.py
@@ -1,10 +1,14 @@
-from openpyxl import load_workbook
+# from openpyxl import load_workbook
 import constants as const
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
 
 def update_excel(case_data, comment):
-    workbook = load_workbook(const.file_path)
-    worksheet = workbook.active
-    max_row = worksheet.max_row
+
     excel_data = []
     excel_data.append(case_data['caseNumber'])
     excel_data.append(case_data['case_link'])
@@ -12,9 +16,35 @@ def update_excel(case_data, comment):
     excel_data.append(case_data['priority'])
     excel_data.append(case_data['subject'])
     excel_data.append(comment)
-    worksheet.append(excel_data)
-    workbook.save(const.file_path)
 
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('./credentials/credentials.json', const.scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    list = [excel_data]
+    resource = {
+        "majorDimension": "ROWS",
+        "values": list
+    }
+    service.spreadsheets().values().append(
+        spreadsheetId=const.spreadsheet_id,
+        range=const.sheet_range,
+        body=resource,
+        valueInputOption="USER_ENTERED"
+    ).execute()
 
 
 
